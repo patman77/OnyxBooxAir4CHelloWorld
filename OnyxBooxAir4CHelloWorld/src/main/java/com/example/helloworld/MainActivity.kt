913f39d833667ee6b8209c971baaf7ae11af22c5
Patrick Klie <info@patman-industries.com>
2025-09-14 18:26:50 +0200

    Initial commit

---
+++ OnyxBooxAir4CHelloWorld/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                LogoScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun LogoScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = "https://www.dunreeb.com/wp-content/uploads/2020/01/cropped-Dunreeb-Logo.png",
+            contentDescription = "Dunreeb Logo"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LogoPreview() {
+    HelloWorldTheme {
+        LogoScreen()
+    }
+}
